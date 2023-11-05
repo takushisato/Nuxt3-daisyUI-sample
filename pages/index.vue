e8246f4f3d0828687bb46ed871b3b5504a3e2555
taku820801 <takuchanpower@gmail.com>
2023-11-05 15:12:35 +0900

    daisy.UIインストール

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="font-bold text-red-300">テスト</h1>
+  <button class="btn btn-neutral">Neutral</button>
+  <button class="btn btn-secondary">Secondary</button>
+</template>
+<script></script>
+<style></style>
